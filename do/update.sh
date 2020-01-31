55f2858fb93f5a3ab9a10ff440473227f373c384
Alexander Gorelick <gorelica@mskcc.org>
2020-01-31 18:47:04 -0500

    updating gitignore

--- do/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-in=$1
-out=$2
-
-cmd="R -e \"rmarkdown::render('~/lab/repos/compound-mutations/do/"$in"', output_file = '~/lab/repos/compound-mutations/do/"$out"')\""
-echo $cmd
-eval $cmd
