d131b921bf1290c2108c5dc0edcbf6368abc8d66
Alexander Gorelick <gorelica@mskcc.org>
2020-02-01 12:05:21 -0500

    updated and improved workflow for cancer-type permutations.

--- r/permute_cancertype_data.R
+++ r/permute_cancertype_data.R
@@ -10,11 +10,12 @@ dir_out <- here('data/cancertype_permutation_tests')
 
 ## load sampleid and gene-symbols from mutation data
 message('loading data ...')
-d <- fread(here('data/data_mutations.txt'),select=c('Tumor_Sample_Barcode','Hugo_Symbol','exclude','putative_resistance_mutation','metamaintype'))
+d <- fread(here('data/data_mutations.txt.gz'),
+           select=c('Tumor_Sample_Barcode','Hugo_Symbol','exclude','putative_resistance_mutation','metamaintype'))
 d <- d[order(Tumor_Sample_Barcode, Hugo_Symbol),]
 
 message('Starting permutations for [',cancertype,'] at ',as.character(date()))
-clin <- fread(here('data/data_clinical.txt'))
+clin <- fread(here('data/data_clinical.txt.gz'))
 clin <- clin[exclude==F & metamaintype %in% cancertype,]
 N_samples <- nrow(clin)
 d <- d[exclude==F & putative_resistance_mutation==F,]
@@ -36,11 +37,11 @@ run_permutations <- function(batch,label,d,N_samples,R) {
         x <- as.data.frame.matrix(x)
         ps <- permatswap(x, times = R, burnin = 100, thin = 10, mtype = "count", fixedmar='both', shuffle='both')
         f = function(m) {
-            compounds <- sum(rowSums(m > 1) > 0)
-            list(compounds=compounds)
+            composites <- sum(rowSums(m > 1) > 0)
+            list(composites=composites)
         }
         l <- rbindlist(lapply(ps$perm, f))
-        l$prop <- l$compounds / N_samples
+        l$prop <- l$composites / N_samples
         l$batch <- batch
         write.tsv(l, fout)
         rm(ps)
--- r/run_compound_rate_permutation_test_per_cancertype.R
+++ r/run_compound_rate_permutation_test_per_cancertype.R
@@ -1,24 +1,26 @@
-
-require(vegan)
-require(data.table)
-require(rpkg)
-require(here)
-
+source(here::here('r/prerequisites.R'))
 
 ## run cancertype permutation tests with fixed density
-clin <- fread(here('data/data_clinical.txt'))
+clin <- fread(here('data/data_clinical.txt.gz'))
 clin <- clin[exclude==F,]
 tumortype_tbl <- table.freq(clin$metamaintype)
 tumortypes <- tumortype_tbl$value
 tumortypes <- tumortypes[tumortypes!='Other']
 
-dir.create(here('data/lsf'))
-dir.create(here('data/cancertype_permutation_tests'))
+dir.create(here('data/lsf'),showWarnings=F)
+dir.create(here('data/cancertype_permutation_tests'),showWarnings=F)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# launch LSF jobs to run permutation test
+# per tumortype. This can take days!
+# The while() loop below will merge/save results
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 launch_job <- function(type) {
     id <- gsub(' ','_',tolower(type))
     rscript <- here('r/permute_cancertype_data.R')
-    cmd <- paste0('Rscript ',rscript," '",type)
+    cmd <- paste0('Rscript ',rscript," '",type,"'")
     mem <- 32
     cores <- 24
     job_id <- paste0('perm_',id)
@@ -29,14 +31,9 @@ launch_job <- function(type) {
     message(bsub_cmd)
     system(bsub_cmd, intern=F, wait=T)
 }
-trash <- lapply(tumortypes[1:length(tumortypes)], launch_job)
 
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# merge/save results
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-clin <- fread(here('data/data_clinical.txt'))
+## load and merge the results, check if any cancer types did not finish
+clin <- fread(here('data/data_clinical.txt.gz'))
 clin <- clin[exclude==F,]
 tumortype_tbl <- table.freq(clin$metamaintype)
 tumortypes <- tumortype_tbl$value
@@ -57,27 +54,43 @@ prev$type <- sapply(prev$type, f)
 ctypes <- gsub(' ','_',tolower(tumortypes))
 map <- data.table(tumortype=tumortypes,type=ctypes)
 prev <- merge(prev, map, by='type', all.x=T)
-tbl <- table.freq(prev$tumortype)
-donetypes <- tbl$value[tbl$N==10000]
-prev <- prev[tumortype %in% donetypes,]
+if(nrow(prev)==0) {
+    todo <- tumortypes
+} else { 
+    tbl <- table.freq(prev$tumortype)
+    donetypes <- tbl$value[tbl$N==10000]
+    prev <- prev[tumortype %in% donetypes,]
+    types_not_finished <- tbl$value[tbl$N < 10000]
+    types_not_yet_run <- tumortypes[!tumortypes %in% tbl$value]
+    todo <- c(types_not_finished, types_not_yet_run)
+}
 
+
+## If there were any tumortypes which either were not started, or did not finish, run them here.
+if(length(todo)>0) {
+    msg <- paste('These cancer types did not finish:',paste(sort(todo),collapse=', '),'. Re-launching LSF jobs to finish them.')
+    trash <- lapply(todo, launch_job)
+    stop('Quitting. Resume when all LSF jobs complete.')
+}
+
+
+## once permutations tests for each cancer type are finished, merge and annotate the results
 summarize_type <- function(query.tumortype,prev,tumortype_tbl,d) {
     message(query.tumortype)
     N_samples <- tumortype_tbl$N[tumortype_tbl$value==query.tumortype]
     m <- as.data.frame.matrix(xtabs(~Tumor_Sample_Barcode + Hugo_Symbol,data=d[metamaintype==query.tumortype]))
-    compounds <- sum(rowSums(m > 1) > 0)
-    obs <- compounds / N_samples
+    composites <- sum(rowSums(m > 1) > 0)
+    obs <- composites / N_samples
     x <- prev$prop[prev$tumortype==query.tumortype] 
     p.value = sum(x >= obs)/length(x)
     mu <- mean(x)
     qs <- as.numeric(quantile(x,c(0.025,0.975)))
     lor <- log2(obs/mu)
-    list(tumortype=query.tumortype,x=compounds,N=N_samples,obs=obs,mu=mu,lwr=qs[1],upr=qs[2],p.value=p.value,logOR=lor)
+    list(tumortype=query.tumortype,x=composites,N=N_samples,obs=obs,mu=mu,lwr=qs[1],upr=qs[2],p.value=p.value,logOR=lor)
 }
 
-d <- fread(here('data/data_mutations.txt'))
+d <- fread(here('data/data_mutations.txt.gz'))
 d <- d[exclude==F & putative_resistance_mutation==F,]
-
 l <- lapply(donetypes, summarize_type, prev, tumortype_tbl, d)
 ll <- rbindlist(l)
 ll <- ll[order(obs)]
