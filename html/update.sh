7c1650bbad54c05c63cbe0297f8b71caeecd9ab2
Alexander Gorelick <gorelica@mskcc.org>
2020-01-31 23:51:15 -0500

    removed update.sh dev script

--- html/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-in=$1
-
-cmd="R -e \"rmarkdown::render('~/lab/repos/composite-mutations/pdf/"$in".Rmd', output_file = '~/lab/repos/composite-mutations/pdf/"$in".pdf')\""
-echo $cmd
-eval $cmd
